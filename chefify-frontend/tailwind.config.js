c59d428faa0a4ec1f4725b3e5e63d30bd066f20d
UdeshaRashmi <rashmiparanamana1999@gmail.com>
2025-11-24 22:20:28 +0530

    update

--- chefify-frontend/tailwind.config.js
+++ chefify-frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
+module.exports = {
+ content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
@@ -8,4 +8,3 @@ export default {
   },
   plugins: [],
 }
-
